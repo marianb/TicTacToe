8b22788860836474dcb6feddf229e65b80bc086c
Marian Beneraf <mbeneraf@yahoo.com>
2017-04-30 01:20:26 -0400

    My second, updated commit!

--- src/cse310/project/Server.java
+++ src/cse310/project/Server.java
@@ -12,27 +12,25 @@ package cse310.project;
 import java.io.*;
 import java.net.*;
 public class Server {
-    public static void main(String[]args){               
+    public static void main(String[]args) throws IOException{               
         int portNumber = 2000;
-        
-        while (true){
-            try{
-                ServerSocket first = new ServerSocket(portNumber); 
+        int uniquePlayerID = 0;
+        //creates our server!
+        ServerSocket theServer = new ServerSocket(portNumber); 
+        try{
+            while(true){
                 //Accepts client's request for a connection
-                ////System.out.println("Helo1");
-                Socket connect = first.accept();
-                ////System.out.println("Helo2");
+                Socket connect = theServer.accept();
                 //Reads input from socket
                 BufferedReader in = new BufferedReader
                 (new InputStreamReader(connect.getInputStream()));
                 //GameState g = new GameState();
-                
+            
                 //Output stream of data from socket
                 DataOutputStream out = new DataOutputStream(connect.getOutputStream());
             } 
-            catch(Exception e){
+        }catch(IOException e){
                 
-            }
         }
     }
 }
